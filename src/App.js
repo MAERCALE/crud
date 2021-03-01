319fdd1f29819f98034d15fd60462455d0ee7c1a
mecle <mariolazomunoz@gmail.com>
2021-03-01 12:27:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     <div className="container mt-5">
       <h1>Tareas</h1>
       <hr/>
+      <div className="row">
+      <div className= "col-8">
+        <h4 className="text-center">Lista de Tareas</h4>
+      </div>
+      <div className= "col-4">
+      <h4 className="text-center">Formulario</h4>
+      </div>
+      </div>
     </div>
   )
 }
